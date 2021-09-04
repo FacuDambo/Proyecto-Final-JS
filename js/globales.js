0e697b97f332aaec8b8f866785ee5f430426e838
FacuDambo <dambolenafacundo@gmail.com>
2021-09-04 18:39:36 -0300

    Proyecto final de JS

---
+++ js/globales.js
@@ -0,0 +1,5 @@
+//VARIABLES GLOBALES
+let carrito = [];
+const productos = [];
+const productosFiltrados = [];
+const categorias = ["PLACAS DE VIDEO", "MOTHERBOARDS", "PROCESADORES", "MEMORIA RAM", "GABINETES"]
